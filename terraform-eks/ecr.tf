fe9e74f8933e02de3430ee85f785d332b15380c6
Mohamed Saleh <mohamedsaleh.elec@gmail.com>
2025-03-19 01:59:32 +0100

    update ecr.tf

--- terraform-eks/ecr.tf
+++ terraform-eks/ecr.tf
@@ -1,5 +1,6 @@
 resource "aws_ecr_repository" "hello_world_ecr" {
   name = var.ecr_repository_name
+  force_delete = true  # Ensures repository is deleted even if images exist
 
   image_scanning_configuration {
     scan_on_push = true
